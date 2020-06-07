129183e63cc7507fa8c67a4ca0f0d1bf2a72b245
Fred Morris <consulting@m3047.net>
2020-06-07 16:48:12 -0700

    Add pydoc.

--- forwarder.py
+++ forwarder.py
@@ -1,4 +1,31 @@
 #!/usr/bin/python3
+# Copyright (c) 2020 by Fred Morris Tacoma WA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""TCP-only DNS Forwarder.
+
+Please read the README. Usage:
+
+    forwarder.py <loopback-address> <dns-server-address> &
+    
+The above will run the script in the background. dns-server-address should be one of your
+configured local caching resolvers (I don't recommend 8.8.8.8).
+
+After running the script edit your network settings and change your resolver to loopback-address.
+
+loopback-address will typically be 127.0.0.1 for IP4 or ::1 for IP6.
+"""
 
 import sys
 import asyncio
